c82a82406e5433379000967923ee0d09d18e805e
Brooklyn Zelenka <be.zelenka@gmail.com>
2019-08-06 23:29:48 -0700

    Better error page

--- app.js
+++ app.js
@@ -6,7 +6,6 @@ var logger = require('morgan');
 var fileUpload = require('express-fileupload');
 
 var indexRouter = require('./routes/index');
-var usersRouter = require('./routes/users');
 
 var app = express();
 
@@ -23,7 +22,6 @@ app.use(express.static(path.join(__dirname, 'public')));
 app.use(fileUpload());
 
 app.use('/', indexRouter);
-app.use('/users', usersRouter);
 
 // catch 404 and forward to error handler
 app.use(function(req, res, next) {
--- routes/index.js
+++ routes/index.js
@@ -1,23 +1,35 @@
 const axios = require('axios');
 const express = require('express');
 const router = express.Router();
+const querystring = require('querystring');
 
 const ipfsProvider = process.env.INTERPLANETARY_FISSION_URL || 'http://localhost:1337';
 const username = process.env.INTERPLANETARY_FISSION_PASSWORD || "ca2c70bc13298c5109ee";
 const password = process.env.INTERPLANETARY_FISSION_USERNAME || "VJ_u+fou!VRF9+MBOeyAw4LQnrqD4*C1sHcl2Cgl2NAjV.XAiAspSv+9!w";
 const auth = {username, password};
+const title = "The FISSION Mini Pinboard";
 
 router.get('/', function(req, res, next) {
-  const title = "FISSION IPFS Mini Gallery";
-
   axios
     .get(ipfsProvider + '/ipfs/cids/', {auth})
     .then(({data: cids}) => res.render('index', {cids, ipfsProvider, title}))
     .catch(console.error);
 });
 
+router.post('/pin', function(req, res) {
+  const {cid} = req.body;
+
+  axios
+    .put(ipfsProvider + '/ipfs/' + cid, {}, {auth})
+    .catch((error) => {
+      console.error(error.stack);
+      res.status(500).render('error', {error, message: 'Unable to pin CID ' + cid + "."});
+    })
+    .then(() => res.redirect('/'));
+});
+
 router.post('/upload', function(req, res) {
-  if (Object.keys(req.files).length == 0) {
+  if (Object.keys(req.files).length === 0) {
     return res.status(400).send('No files were uploaded.');
   }
 
@@ -26,7 +38,10 @@ router.post('/upload', function(req, res) {
 
   axios
     .post(ipfsProvider + '/ipfs?name=' + name, data, {auth, headers})
-    .catch(console.error)
+    .catch((error) => {
+      console.error(error.stack);
+      res.status(500).render('error', {error, message: 'Unable to upload. Please try again.'});
+    })
     .then(() => res.redirect('/'));
 });
 
