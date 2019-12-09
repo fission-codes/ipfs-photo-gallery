c125950278bec5e2c6a600240d5f3f19c2858a4e
Boris Mann <boris@bmannconsulting.com>
2019-12-09 15:14:42 -0800

    Set runfission as default URL

--- src/ipfs/getPhotoGallery.ts
+++ src/ipfs/getPhotoGallery.ts
@@ -1,6 +1,6 @@
 import axios from 'axios';
 
-export const ipfsProvider = process.env.INTERPLANETARY_FISSION_URL || 'https://hostless.dev';
+export const ipfsProvider = process.env.INTERPLANETARY_FISSION_URL || 'https://runfission.com';
 const username = process.env.INTERPLANETARY_FISSION_USERNAME;
 const password = process.env.INTERPLANETARY_FISSION_PASSWORD;
 const auth = username && password && {username, password};
