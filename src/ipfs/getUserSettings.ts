cafccbf641186eba2dd4aab709fdcceb5ccdf9ce
Boris Mann <boris@bmannconsulting.com>
2019-12-09 15:18:58 -0800

    Removed REACT_APP from constant

--- src/ipfs/getUserSettings.ts
+++ src/ipfs/getUserSettings.ts
@@ -3,7 +3,7 @@ import { content } from '@fission-suite/client'
 export type CID = string;
 
 export const DEFAULT_CID =
-    process.env.REACT_APP_DEFAULT_CID ||
+    process.env.DEFAULT_CID ||
     "QmUWWqCNSdZmus7mc52um5cpqUi1CaE97AzBTY7iWfBXV9";
 
 export const loadUserSettings = async (cid: CID) => {
