40ddf3d7d164b2b245f29c037cd39f94b828b52b
Pat Dryburgh <hello@patdryburgh.com>
2020-08-17 15:48:38 -0700

    start implementing webnative sdk

--- src/ipfs/getPhotoGallery.ts
+++ src/ipfs/getPhotoGallery.ts
@@ -1,19 +1,19 @@
-import axios from 'axios';
-import {Auth} from "@fission-suite/client";
-
-export const ipfsProvider = process.env.REACT_APP_INTERPLANETARY_FISSION_URL || 'https://hostless.dev';
-
-const getPhotoGalleryCids = async (auth: Auth) => {
-    if (auth) {
-        try {
-            return await axios.get(ipfsProvider + '/ipfs/cids/', {auth});
-        } catch (error) {
-            console.log(error);
-        }
-    }
-};
-
-export const getPhotos = async (auth: Auth) => {
-    const photos = await getPhotoGalleryCids(auth);
-    return photos && photos.data;
-};
+// import axios from 'axios';
+// import {Auth} from "@fission-suite/client";
+//
+// export const ipfsProvider = process.env.REACT_APP_INTERPLANETARY_FISSION_URL || 'https://hostless.dev';
+//
+// const getPhotoGalleryCids = async (auth: Auth) => {
+//     if (auth) {
+//         try {
+//             return await axios.get(ipfsProvider + '/ipfs/cids/', {auth});
+//         } catch (error) {
+//             console.log(error);
+//         }
+//     }
+// };
+//
+// export const getPhotos = async (auth: Auth) => {
+//     const photos = await getPhotoGalleryCids(auth);
+//     return photos && photos.data;
+// };
