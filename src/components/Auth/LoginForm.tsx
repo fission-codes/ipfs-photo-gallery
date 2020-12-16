e93814920406f4f57656e46ade71f093ce809c20
Pat Dryburgh <hello@patdryburgh.com>
2020-12-16 15:37:07 -0500

    Improve log in form

--- src/components/Auth/LoginForm.tsx
+++ src/components/Auth/LoginForm.tsx
@@ -1,6 +1,6 @@
 import * as React from 'react';
 import * as sdk from 'webnative';
-import { Box, Button, Card, CardContent, CardMedia, Grid, Typography } from '@material-ui/core';
+import { Box, Button, CardContent, CardMedia, Grid, Typography } from '@material-ui/core';
 import CssBaseline from '@material-ui/core/CssBaseline';
 import Icon from './undraw_image_upload_wqh3.svg'
 import { makeStyles } from '@material-ui/core/styles';
@@ -22,6 +22,13 @@ const LoginForm: React.FC = () => {
             backgroundSize: '96%',
             marginTop: theme.spacing(4)
         },
+        subtitle: {
+            color: theme.palette.text.secondary,
+            marginBottom: theme.spacing(3)
+        },
+        card: {
+            marginTop: theme.spacing(-4)
+        }
     }));
     const classes = useStyles();
 
@@ -30,21 +37,21 @@ const LoginForm: React.FC = () => {
             <CssBaseline/>
             <Box className={classes.box}>
                 <Grid container={true} justify={'center'} alignItems={'center'}>
-                    <Grid item={true} sm={6} md={4}>
-                        <Card>
-                            <CardMedia
-                                className={classes.media}
-                                image={Icon}
-                                title="IPFS Photo Gallery"
-                            />
-                            <Box px={2} pb={2}>
-                                <CardContent>
-                                    <Typography variant={'h5'} component={'h1'}>IPFS Photo Gallery</Typography>
-                                    <Typography variant={'subtitle1'} component={'h2'} gutterBottom={true}>Pin your photos to the distributed web!</Typography>
-                                    <Button variant={'contained'} color={'primary'} onClick={() => state !== undefined ? sdk.redirectToLobby(state.permissions) : undefined}>Log In to Fission</Button>
-                                </CardContent>
-                            </Box>
-                        </Card>
+                    <Grid item={true} xs={12} sm={9} lg={8}>
+                        <CardMedia
+                            className={classes.media}
+                            image={Icon}
+                            title="IPFS Photo Gallery"
+                        />
+                    </Grid>
+                    <Grid item={true} sm={9} md={7}>
+                        <Box px={2} pb={2} className={classes.card}>
+                            <CardContent>
+                                <Typography variant={'h3'} component={'h1'} >Fission Photos</Typography>
+                                <Typography variant={'h5'} component={'h2'} gutterBottom={true} className={classes.subtitle}>Your photos on the distributed web</Typography>
+                                <Button variant={'contained'} color={'primary'} onClick={() => state !== undefined ? sdk.redirectToLobby(state.permissions) : undefined}>Log In to Fission</Button>
+                            </CardContent>
+                        </Box>
                     </Grid>
                 </Grid>
             </Box>
